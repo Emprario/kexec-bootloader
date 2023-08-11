0d243e97176d08b68acd5ed9c829f64d8d911170
Emprario <pierre.cameleri@free.fr>
2023-08-11 18:54:12 +0200

    Add Initramfs support and changes name Initramfs support via a script run as root in /mkinitscr/TARGET/mkinitramfs.sh with full control over the folder BROOT and BUILD_PATH Change some variables names to closer to the naming convention of ArchLinux and proper naming convention in general Split the main root directory into two: BROOT the root, and BUILD_PATH the output folder for the result of the builds

--- build.sh
+++ build.sh
@@ -2,23 +2,26 @@
 
 set -e
 
-VERSION="6.4.9"
-KREV="arch1"
+KVERSION="6.4.9"
+AREV="arch1"
 CREV="1"
-KVERSION="$VERSION-$KREV-$CREV"
-NO_REV_KVERSION="$VERSION-$KREV"
-CONFIG_VERSION="$VERSION.$KREV-$CREV"
+BVERSION="$KVERSION-$AREV-$CREV"
+KERNEL_VERSION="$KVERSION-$AREV"
+CONFIG_VERSION="$KVERSION.$AREV-$CREV"
 
-REMOTE_KERNEL="https://github.com/archlinux/linux/archive/refs/tags/v$NO_REV_KVERSION.tar.gz"
+REMOTE_KERNEL="https://github.com/archlinux/linux/archive/refs/tags/v$KERNEL_VERSION.tar.gz"
 REMOTE_CONFIG="https://gitlab.archlinux.org/archlinux/packaging/packages/linux/-/raw/$CONFIG_VERSION/config"
 
 BROOT="$PWD"
+BUILD_PATH=$BROOT/output
 SRC_LINUX=$BROOT/linux
+MODULES_FOLDER=$SRC_LINUX/modules
+
+
 #TMPDIR=$(mktemp -d) # Accessible var in $TMPDIR
 
 
-TARGETs=("standard" "chgconfig" "rescue") 
-
+TARGETs=("standard" "chgconfig" "rescue" "clean") 
 
 source functions.sh
 
@@ -28,7 +31,7 @@ usage () {
   echo "  -h | --help       Print help"
   echo "  -p | --print-var  Print input variables"
   echo 
-  #echo "    KVERSION is a variable that is overwritten the default ($KVERSION) if specifed."
+  #echo "    BVERSION is a variable that is overwritten the default ($BVERSION) if specifed."
   echo "    TARGET [required] is a keyword to specified what you want to build ;"
   echo "    List of TARGET: ${TARGETs[*]}"
   exit 0
@@ -36,7 +39,7 @@ usage () {
 
 print_var () {
   infop "TARGET=$TARGET"
-  infop "KVERSION=$KVERSION"
+  infop "BVERSION=$BVERSION"
   exit 0
 }
 
@@ -54,30 +57,38 @@ main () {
     exit 0
   fi
   
+  if [[ $TARGET == "clean" ]];then
+    clean
+    exit 0
+  fi
+  
+  build
+  install_modules
+  mkinit
   build
 }
 
 download () {
   cd $BROOT
   
-  if ! [ -d v$KVERSION  ];  then
-    mkdir -p v$KVERSION
-    cd v$KVERSION
-    infop "Use archive directory: $BROOT/v$KVERSION"
+  if ! [ -d v$BVERSION  ];  then
+    mkdir -p v$BVERSION
+    cd v$BVERSION
+    infop "Use archive directory: $BROOT/v$BVERSION"
 
 
     infop "Dowloading using curl ..."
 
-    curl -L $REMOTE_KERNEL -o kernel-$NO_REV_KVERSION.tar.gz || error "Cannot download kernel : $REMOTE_KERNEL" 1
-    curl -L $REMOTE_CONFIG -o config-$KVERSION || error "Cannot download config : $REMOTE_CONFIG" 1
+    curl -L $REMOTE_KERNEL -o kernel-$KERNEL_VERSION.tar.gz || error "Cannot download kernel : $REMOTE_KERNEL" 1
+    curl -L $REMOTE_CONFIG -o config-$CONFIG_VERSION || error "Cannot download config : $REMOTE_CONFIG" 1
     
     infop "Setting up enviroment ..."
 
-    tar xf kernel-$NO_REV_KVERSION.tar.gz
+    tar xf kernel-$KERNEL_VERSION.tar.gz
     cd ..
     rm -f linux config
-    ln -s v$KVERSION/linux-$NO_REV_KVERSION linux
-    ln -s v$KVERSION/config-$KVERSION config
+    ln -s v$BVERSION/linux-$KERNEL_VERSION linux
+    ln -s v$BVERSION/config-$CONFIG_VERSION config
 
     infop "Build env sucessfully setup !"
   else
@@ -86,14 +97,14 @@ download () {
 }
 
 prepare_env () {
-  cd $BROOT
+  cd $BUILD_PATH
   
   infop "Prepare build environement"
   
-  cp config config.appended
+  cp $BROOT/config config.appended
   
-  if ! [ -f overlays/$TARGET/no_overlay.conf ]; then
-    cat overlays/$TARGET/*.conf >> config.appended
+  if ! [ -f $BROOT/overlays/$TARGET/no_overlay.conf ]; then
+    cat $BROOT/overlays/$TARGET/*.conf >> config.appended
   fi
   
   cp config.appended $SRC_LINUX/.config
@@ -106,9 +117,9 @@ menuconfig () {
   
   make menuconfig
   
-  diff -y --suppress-common-lines $BROOT/config .config > $BROOT/config.diff || echo
+  diff -y --suppress-common-lines $BROOT/config .config > $BUILD_PATH/config.diff || /bin/true
   
-  infop "You can find the diff in $BROOT/config.diff"
+  infop "You can find the diff in $BUILD_PATH/config.diff"
 }
 
 build () {
@@ -124,10 +135,49 @@ build () {
   infop "Kernel $KERNEL_STRING sucessfully built !"
 }
 
+mkinit () {
+  cd $BROOT/mkinitscr/$TARGET
+  
+  MKINIT=$BROOT/mkinitscr/$TARGET/mkinitramfs.sh
+  
+  infop "Building initramfs using : $MKINIT"
+  
+  if ! sudo bash $MKINIT $BROOT $BUILD_PATH; then
+    error "Something went wrong with MKINIT script !" 1
+  fi
+  
+  cp $BUIB_PATH/initramfs.cpio.xz $SRC_LINUX
+}
 
+clean () {
+  cd $SRC_LINUX
+  
+  make clean
+}
 
+install_modules() {
+  cd $SRC_LINUX
 
+  # Create empty modules folder
+  sudo rm -rf $BUILD_PATH/modules.tar.xz
 
+  make -j"$(nproc)" modules_install INSTALL_MOD_PATH=$MODULES_FOLDER INSTALL_MOD_STRIP=1
+
+  cd $MODULES_FOLDER/lib/modules
+  # Remove broken symlinks
+  rm -rf */build
+  rm -rf */source
+
+  # Create an archive for the modules
+  tar -cv --use-compress-program="xz -9 -T0" -f $BUILD_PATH/modules.tar.xz *
+  infop "Modules archive created."
+
+}
+
+post () {
+  export -n $BROOT
+  export -m $BUILD_PATH
+}
 
 TARGET=""
 PRINT_VAR=false
@@ -166,6 +216,11 @@ if $PRINT_VAR; then
   print_var
 fi
 
-main
+if ! main; then
+  post
+  exit 1
+fi
+
+post
 
 exit 0
---
+++ mkinitscr/standard/mkinitramfs.sh
@@ -0,0 +1,155 @@
+#!/bin/bash
+
+BROOT=$1
+BUILD_PATH=$2
+
+SRC="$PWD"
+INITRAMFS_PATH=$BUILD_PATH/initramfs
+INIT_SCRIPT=$SRC/init
+MODULES=$BUILD_PATH/modules.tar.xz
+CPIO_ARCHIVE=$BUILD_PATH/initramfs.cpio.xz
+#ALPINE vars as ALP
+ALP_ARCHIVE=alpine-minirootfs-3.18.2-x86_64.tar.gz
+ALP_ARCHIVE_PATH=$BUILD_PATH/$ALP_ARCHIVE
+ALP_URL=https://dl-cdn.alpinelinux.org/alpine/v3.18/releases/x86_64/$ALP_ARCHIVE
+ALP_UNUSED_APK="ca-certificates alpine-keys apk-tools"
+ALP_USEFUL_APK="bash util-linux linux-firmware-amdgpu"
+#ALP_EDGETE_APK=""
+ALP_DEVELP_APK="vim nano tree"
+ALP_COMDEV_APK="vboot-utils cgpt"
+PATH="/bin:/sbin:/usr/bin:/usr/sbin"
+
+CHROOT="chroot $INITRAMFS_PATH"
+
+# Exit on errors
+set -e
+
+source $BROOT/functions.sh
+
+clean () {
+
+  infop "Make clean image"
+  # make a clean image
+  umount -Rl $INITRAMFS_PATH/* || /bin/true
+  rm -rf $INITRAMFS_PATH
+  mkdir $INITRAMFS_PATH
+}
+
+# Create basic root
+basic_root () {
+  cd $INITRAMFS_PATH
+  
+  mkdir --parents ./{usr,bin,dev,etc,lib,mnt/root,proc,root,sbin,sys,tmp}
+  mknod -m 622 dev/console c 5 1
+  mknod -m 622 dev/tty0 c 4 1
+  mknod -m 622 dev/ttyS0 c 4 64
+  cp $INIT_SCRIPT .
+  
+  # Make usr looks like root
+  cd $INITRAMFS_PATH/usr
+  mkdir --parents ./{local,state,share,src,bin,sbin,lib}
+  cd ..
+  
+  if $ADDMOD;then
+    # extract modules to /lib/modules
+    mkdir ./lib/modules
+    tar xpf $MODULES -C lib/modules
+  fi
+  
+  
+  if $DEVMODE;then
+    # enable devmode in initramfs
+    touch /etc/devmode
+  fi
+  
+}
+
+downtract_alpine () {
+  cd $BUILD_PATH
+
+  infop "Downloading alpine minimal rootfs"
+  if [[ ! -d $ALP_ARCHIVE_PATH ]] ; then
+    curl -L $ALP_URL -o $ALP_ARCHIVE_PATH || error "Failed to download using curl, check you own curl, check network connection." 1
+  else
+    warning "Alpine rootfs already downloaded"
+  fi
+
+  tar -xhf $ALP_ARCHIVE_PATH -C $INITRAMFS_PATH
+}
+
+
+alp_manage_apk () {
+  cd $INITRAMFS_PATH
+  
+  infop "Set up internet access"
+  cp /etc/resolv.conf ./etc/resolv.conf
+  
+  infop "Add apks"
+  $CHROOT apk update
+  $CHROOT apk add $ALP_USEFUL_APK
+  #$CHROOT apk add $ALP_EDGETE_APK --repository=http://dl-cdn.alpinelinux.org/alpine/edge/testing/
+  
+  if $DEVAPK;then
+    infop "Installing extra apks"
+    $CHROOT apk add $ALP_DEVELP_APK
+    $CHROOT apk add $ALP_COMDEV_APK --repository=http://dl-cdn.alpinelinux.org/alpine/edge/community/
+  else
+    infop "Cleaning unused apks"
+    $CHROOT apk del $ALP_UNUSED_APK
+  fi
+}
+
+
+
+# extract firmware blob needed
+extract_blobs () {
+  cd $INITRAMFS_PATH
+  
+  cp -r /lib/firmware/amdgpu ./lib/firmware
+}
+
+# add all scripts in scripts.d into /lib/scripts
+install_scripts () {
+  cd $INITRAMFS_PATH  
+  
+  if [ -d $SRC/scripts.d/ ];then
+    mkdir ./lib/scripts
+    cp $SRC/scripts.d/* ./lib/scripts/
+  fi
+}
+
+# compile cpio archive
+compile_cpio () {
+  cd $INITRAMFS_PATH
+
+  rm -f $CPIO_ARCHIVE
+  find .  | cpio -ov --format=newc | xz --check=crc32 --lzma2=dict=512KiB -ze -9 -T$(nproc) > $CPIO_ARCHIVE
+
+}
+
+ADDMOD=true
+DEVAPK=false
+DEVMODE=false
+
+for opt in $@
+do
+  case $opt in
+    --no-modules)
+      ADDMOD=false
+      ;;
+    --devapk)
+      DEVAPK=true
+      ;;
+    --devmode)
+      DEVMODE=true
+      ;;
+  esac
+done
+
+clean
+basic_root
+downtract_alpine
+alp_manage_apk
+#extract_blobs
+install_scripts
+compile_cpio
