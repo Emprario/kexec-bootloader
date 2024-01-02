33ba96920f44b2b436719b145decedcf6d622d03
Emprario <pierre.cameleri@free.fr>
2024-01-02 11:00:25 +0000

    change load-grub to use global variables

--- mkinitscr/kaboot/usr/lib/kaboot/main.sh
+++ mkinitscr/kaboot/usr/lib/kaboot/main.sh
@@ -12,13 +12,9 @@ fi
 . $srcPATH/print_menu.sh
 . $srcPATH/scan_boot.sh
 
-#get_scan # return $OPTIONS
+ROOT=/mnt
+GRUBROOT=/boot/grub
 
-#OPTIONS_useNAME=""
-#for option in $OPTIONS
-#do
-#  OPTIONS_useNAME="$(echo $option | cut -f4 -d"/")_on_/dev/$(echo $option | cut -f3 -d"/") $OPTIONS_useNAME"
-#done
-#selection "Selection:" "$OPTIONS_useNAME"
+. $srcPATH/kexec-load-grub
 
-$srcPATH/kexec-load-grub --grub /mnt/boot/grub --mnt /mnt
+main "$@"
