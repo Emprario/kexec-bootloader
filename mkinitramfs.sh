75b22804406d196852ad4830c6c2df24f190b782
Emprario <pierre.cameleri@free.fr>
2024-01-09 12:46:49 +0000

    change default root to tarball + fix packages + add util-linux

--- mkinitramfs.sh
+++ mkinitramfs.sh
@@ -18,10 +18,14 @@ rm -rf $OUTPATH/rootfs
 mkdir $OUTPATH/rootfs
 cd $OUTPATH/rootfs
 echo Extracting BR rootfs
-cpio -id < $BUILDROOT_PATH/output/images/rootfs.cpio
+tar xpf $BUILDROOT_PATH/output/images/rootfs.tar
 
 ls -l dev -a $BROOT/mkinitscr/$TARGET/* $OUTPATH/rootfs
 cp -a $BROOT/mkinitscr/$TARGET/* $OUTPATH/rootfs
+
+mknod -m 622 dev/console c 5 1
+mknod -m 622 dev/tty0 c 4 1
+mknod -m 622 dev/ttyS0 c 4 64
 
 rm -f $OUTPATH/initramfs.cpio.xz
 find .  | cpio -ov --format=newc | xz --check=crc32 --lzma2=dict=512KiB -ze -9 -T$(nproc) > $OUTPATH/initramfs.cpio.xz
